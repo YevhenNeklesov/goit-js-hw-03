e51bbbc430d90f449331174d388eeca26cea2b4b
YevhenNeklesov <161756863+YevhenNeklesov@users.noreply.github.com>
2024-06-02 13:48:36 +0200

    add task 1-3

---
+++ js/task-3.js
@@ -0,0 +1,19 @@
+
+function filterArray(numbers, value) {
+    
+    const numbersMoreValue = []
+
+    for (const number of numbers) {
+        if (number > value) {
+            numbersMoreValue.push(number)
+        }
+    }
+    
+    return numbersMoreValue
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
